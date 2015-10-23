dbebb6bc755270988885f104b52d6692feddf42d
Jeff Mariconda <jmariconda24@gmail.com>
2015-10-23 15:05:19 -0400

    add gitignore

---
+++ traffic.h
@@ -0,0 +1,14 @@
+#ifndef _TRAFFIC_H_
+#define _TRAFFIC_H_
+
+#include "q.h"
+#include "cart.h"
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <regex.h>
+
+#endif
