fd4598e30d8d8fa8b8d375299d78220a1bff9b14
nishmeni <nmeni@stevens.edu>
2015-10-24 11:00:59 -0400

    Fixed Mem leaks

--- trafficmgr.c
+++ trafficmgr.c
@@ -19,43 +19,47 @@ void argerror() {
  *
  *  arg - string to check
  */
-int check_match(char *arg) {
+int check_match(char *arg, char *reg) {
     regex_t regex;
     int reti;
     char msgbuf[100];
 
     /* Compile regular expression */
-    reti = regcomp(&regex, "^[nsew]*$", 0);
+    reti = regcomp(&regex, reg, 0);
+    
     if (reti) {
-        fprintf(stderr, "Could not compile regex\n");
-        exit(1);
+      fprintf(stderr, "Could not compile regex\n");
+      exit(1);
     }
 
     /* Execute regular expression */
     reti = regexec(&regex, arg, 0, NULL, 0);
+    
     if (!reti) {
         /* match found */
         printf("Expression matched: %s\n", arg);
-        return 1;
+        regfree(&regex);
+	return 1;
     } else if (reti == REG_NOMATCH) {
         /* no match found */
         printf("Expression not matched: %s\n", arg);
+	regfree(&regex);
         return -1;
     } else {
         /* expression match failed */
         regerror(reti, &regex, msgbuf, sizeof(msgbuf));
         fprintf(stderr, "Regex match failed: %s\n", msgbuf);
-        exit(-1);
+        regfree(&regex);
+	exit(EXIT_FAILURE);
     }
-
-    /* Free compiled regular expression */
-    regfree(&regex);
+    
 }
 
 void init(char* arg) {
     int i;
-
     q_init;
+
+
     for(i = 0; arg[i] != '\0'; i++){
         switch (arg[i]) {
 
@@ -73,21 +77,23 @@ void init(char* arg) {
                 break;
         }
     }
-
 }
 
 int main(int argc, char** argv) {
 
-    if (argc == 2 && check_match(argv[1]) > 0) {
-        init(argv[1]);
-        //q_print('n');
-        //q_print('s');
-        //q_print('e');
-        //q_print('w');
-    } else {
-        argerror();
-    }
+  if (argc == 2 && check_match(argv[1],"^[nsew]*$") > 0) {
+    init(argv[1]);
 
-    cleanexit();
-    return 0;
+
+    cleanexit();  
+
+  } else { 
+    
+    argerror();
+  
+  }
+  
+
+
+  return 0;
 }
