5284ac62e3cc68421d9986b46904d59aaeba23c4
nishmeni <nmeni@stevens.edu>
2015-10-24 14:09:35 -0400

    Creates q_deleteOne

--- trafficmgr.h
+++ trafficmgr.h
@@ -15,6 +15,16 @@ typedef struct arg_s {
   char direction;
 } arg_t;
 
+typedef struct s_env {
+
+  int n;
+  int s;
+  int e;
+  int w;
+
+} t_env;
+
+t_env gl_env;
 void cleanexit();
 void argerror();
 int check_match(char*, char*);
